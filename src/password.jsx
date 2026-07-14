5efc19cba4fe1fecd856206fc5af4fac70141093
reactapp-development <umanggg2006@gmail.com>
2026-07-14 15:18:56 +0530

    finaledit

--- src/password.jsx
+++ src/password.jsx
@@ -2,9 +2,40 @@ import React from "react";
 
 const Password = () => {
     return ( 
-        <div className="flex justify-center items-center">
-            <div className="bg-amber-50">
-                <h1>Password change</h1>
+        <div className="flex justify-center items-center w-full h-screen bg-linear-to-br from-gray-600 via-blue-300 to-gray-950">
+            <div className="bg-amber-50 text-black w-[50%] h-[65vh] p-5 rounded-2xl">
+                <div >
+                    <h1 className=" flex justify-center text-lg font-semibold pb-5">Enter new password</h1>
+                    <h2 className="p-2">Your new password  must be different from previous used password.</h2>
+                </div>
+
+                <div className="m-2 text-sm">
+                    <div>
+                        <label>New password</label>
+                        <div>
+                            <input 
+                            type="password"
+                            placeholder="New password"
+                            className="border border-gray-500 p-2 px-15 rounded-lg"
+                        />
+                        </div>
+                        
+                    </div>
+                    <div className="mt-4">
+                        <label >Confirm password</label>
+                        <div>
+                            <input 
+                            type="password"
+                            placeholder="Confirm password"
+                            className="border border-gray-500 p-2 px-15 rounded-lg"
+                        />
+                        </div> 
+                    </div>
+                </div>
+                <div className="flex justify-center py-6">
+                    <button className="bg-blue-400/50 px-10 rounded-xl active:bg-blue-500">Reset password</button>
+                </div>
+                
             </div>
             
         </div>
