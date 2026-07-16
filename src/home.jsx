fb84ae39e3e56e9135434586e8df3e723615790b
reactapp-development <umanggg2006@gmail.com>
2026-07-16 16:09:02 +0530

    do it all

--- src/home.jsx
+++ src/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {motion} from "framer-motion";
-import mars from "./assets/mars.jpg";
+import mountain from "./assets/mountain.jpg";
+import images1 from "./assets/images1.jpg"
 
 const Home = () => {
   return (
@@ -12,7 +13,7 @@ const Home = () => {
       <div
         className="absolute inset-0 bg-cover bg-center opacity-40"
         style={{
-          backgroundImage: 'url("/src/assets/images1.jpg")',
+          backgroundImage: 'url("/src/assets/images.jpg")',
         }}
       ></div>
 
@@ -29,7 +30,7 @@ const Home = () => {
              ease: "easeOut"
              }}    
         >
-            Building the Next Era of 
+            Explore the world
             </motion.h1>
         
         <motion.h1
@@ -42,7 +43,7 @@ const Home = () => {
              }}  
              className="bg-linear-to-r from-green-950 to-blue-500 bg-clip-text text-transparent px-20 py-2"  
         >
-            Space Exploration
+            Where Every Journey begins
             </motion.h1>  
     </div>
 
@@ -56,7 +57,7 @@ const Home = () => {
              ease: "easeOut"
              }}    
         >
-            AstroLink is pioneering the infrastructure for a multi-planetary future. Our advanced starship network is designed to make the cosmos accessible for everyone
+            From  hidden gems to  iconic landmarks, we help  you plan every step of your perfect journey.
             </motion.h2>
     </div>
 
@@ -72,25 +73,36 @@ const Home = () => {
 
       <div className="flex justify-between">
         <div className="pt-20 pl-20">
-            <p>Next Orbit</p>
-            <h1 className="text-5xl font-bold ">Featured Destinations</h1>
+            <p>Next place</p>
+            <h1 className="text-5xl font-bold ">Traveling Destinations</h1>
         </div>
         <div className="pt-36 pr-36 ">
             <h3>Explore high-resolution telemetry data from our furthest outposts and upcoming colonization sites.</h3>
         </div>
       </div>
-      <div  className="pl-20 pt-10 ">
-        <div className="relative">
+      <div  className="pl-20 pt-10  ">
+        <div className="relative flex justify-between">
 
-        <img src={mars}
-         alt="Mars"
+        <img src={mountain}
+         alt="Mountain"
          className="h-[56vh] object-cover rounded-2xl transition-transform duration-500 hover:scale-110 opacity-80"/>
 
          <div className="absolute bottom-6 left-6  text-white ">
 
-          <h2 className="text-2xl font-bold">Mars: The Crimson Frontier</h2>
-          <p className="w-100 pt-4 font-semibold">Witness the Olympus Mons at sunrise and explore the ancient riverbeds of the red planet.</p>
+          <h2 className="text-2xl font-bold">Mountain and a lake</h2>
+          <p className="w-100 pt-4 font-semibold">This is a scenery of a mountain and a lake very beautiful and pretty located in.</p>
 
+          </div>
+          
+            <img src={images1}
+         alt="Mountain"
+         className="h-[56vh] object-cover rounded-2xl transition-transform duration-500 hover:scale-110 opacity-80"/>
+         <div className="absolute bottom-6 left-6  text-white ">
+
+          <h2 className="text-2xl font-bold">Mountain and a lake</h2>
+          <p className="w-100 pt-4 font-semibold">This is a scenery of a mountain and a lake very beautiful and pretty located in.</p>
+
+          
           </div>
          </div>
       </div>
