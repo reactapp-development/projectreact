67a3f0e5d750c3352ae890250d4e2aef69aaa973
reactapp-development <umanggg2006@gmail.com>
2026-07-24 15:19:24 +0530

    addes

--- src/otp.jsx
+++ src/otp.jsx
@@ -1,231 +1,92 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Otp() {
-
   const navigate = useNavigate();
+   const [timeLeft, setTimeLeft] = useState(30);
 
-  const [timeLeft, setTimeLeft] = useState(30);
-
-  const [otp, setOtp] = useState("");
-
+    //OTP check Storage
+    const [otp, setOtp] = useState("");
 
 
-  const handleNext = () => {
-
+  const handleNext = async () => {
     const storedOTP = sessionStorage.getItem("otp");
 
-
     if (otp.trim() === storedOTP) {
+        alert("OTP verification successful");
 
-      alert("OTP verification successful");
-
-      navigate("/password");
+        navigate("/password");
 
     } else {
-
-      alert("Invalid OTP");
-
+        alert("Invalid OTP");
     }
+};
 
-  };
-
-
-
-
-  // Timer
-  useEffect(() => {
-
-    if(timeLeft <= 0) return;
-
+ // Timer
+   useEffect(() => {
+    if (timeLeft <= 0) return;
 
     const timer = setTimeout(() => {
-
-      setTimeLeft(timeLeft - 1);
-
-    },1000);
-
+        setTimeLeft(timeLeft - 1);
+    }, 1000);
 
     return () => clearTimeout(timer);
-
-
-  },[timeLeft]);
-
-
-
-
+}, [timeLeft]);
   return (
-
-    <div
-      className="
-      min-h-screen
-      flex
-      justify-center
-      items-center
-      p-4
-      bg-linear-to-br
-      from-gray-600
-      via-blue-300
-      to-gray-950
-      "
-    >
-
-
-      <div
-        className="
-        w-full
-        sm:w-4/5
-        md:w-1/2
-        lg:w-2/5
-        bg-white/30
-        rounded-xl
-        text-black
-        p-6
-        sm:p-8
-        backdrop-blur-lg
-        "
-      >
-
-
+    <div className="w-full h-screen flex justify-center items-center bg-linear-to-br from-gray-600 via-blue-300 to-gray-950">
+      <div className="bg-white/30 w-[40%] rounded-xl text-black p-8 ">
+        
         <div>
-
-          <h1
-            className="
-            text-xl
-            sm:text-2xl
-            font-semibold
-            text-center
-            "
-          >
+          <h1 className="text-xl font-semibold flex justify-center">
             Verify OTP
           </h1>
-
-
-          <h2
-            className="
-            text-sm
-            mt-4
-            text-center
-            "
-          >
+          <h2 className="text-sm mt-4 px-5">
             Enter the OTP sent to your email
           </h2>
-
-
         </div>
 
-
-
-
-
-        <div className="mt-6">
-
-
+        <div className="mt-6 px-10">
           <label className="font-semibold text-sm">
             OTP
           </label>
+          <label>
+            <p>OTP expires in {timeLeft}s</p>
+          </label>
 
-
-          <p className="text-sm mt-2">
-            OTP expires in {timeLeft}s
-          </p>
-
-
-
-
-          <input
-            type="text"
-            maxLength={6}
-            value={otp}
-            onChange={(e)=>setOtp(e.target.value)}
-            className="
-            w-full
-            mt-3
-            border
-            border-gray-500
-            p-3
-            rounded-lg
-            outline-none
-            text-center
-            tracking-widest
-            text-lg
-            "
-          />
-
-
+          <div className="flex gap-3 mt-2">
+            <input
+                type="text"
+                maxLength={6}
+                value={otp}
+                onChange={(e) => setOtp(e.target.value)}
+                className="border p-2 rounded"
+            />
+          </div>
         </div>
-
-
-
-
-
 
 
         <div className="flex justify-center mt-8">
-
-
           <button
-
-            className="
-            w-full
-            sm:w-auto
-            bg-black
-            text-white
-            font-semibold
-            px-10
-            py-3
-            rounded-xl
-            active:bg-gray-700
-            "
-
-            onClick={handleNext}
-
-          >
-
-            Verify OTP
-
-          </button>
-
-
+          className="bg-black text-white px-20 py-2 rounded-xl active:bg-gray-700"
+          onClick={handleNext}
+        >
+          Verify OTP
+        </button>
         </div>
-
-
-
-
-
 
 
         <div className="text-center mt-5">
-
-
-          <Link
-
+          <Link 
             to="/forgot"
-
-            className="
-            underline
-            text-blue-700
-            "
-
-          >
-
+            className="underline text-blue-700">
             Change Email
-
           </Link>
-
-
         </div>
 
-
-
       </div>
-
-
     </div>
-
   );
-
 }
-
 
 export default Otp;
